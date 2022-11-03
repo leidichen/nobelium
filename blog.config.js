b01f8de1dc918537ec429e6462cedbeca5302ce8
Leidi <leidison@163.com>
2022-11-03 10:16:39 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@ const BLOG = {
   title: 'Leidi\'s Notes',
   author: 'leidichen',
   email: 'leidison.com',
-  link: 'https://nobelium.vercel.app',
+  link: 'https://leidison.com',
   description: 'This gonna be an awesome website.',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
