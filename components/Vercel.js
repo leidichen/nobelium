45508853c1bc3d9402c10f43b0153982ed49ecbb
Leidi <leidison@163.com>
2022-11-03 12:33:35 +0800

    Update Vercel.js

--- components/Vercel.js
+++ components/Vercel.js
@@ -1,7 +1,7 @@
 const Vercel = () => {
   return (
     <a
-      href="https://vercel.com?utm_source=Craigary&utm_campaign=oss"
+      href="https://https://www.lenband.com"
       target="_blank"
       rel="noreferrer"
       aria-label="Vercel"
